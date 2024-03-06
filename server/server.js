9a2f67af90ba4a4f978fa7b0615ed0d1d094a9a4
Mae Kyaw San <maeks@maes-laptop.myfiosgateway.com>
2024-03-06 15:05:11 -0500

    Pull data from MySQL database

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const PORT = process.env.PORT || 8000;
 
 // MeiliSearch API
 import { MeiliSearch } from 'meilisearch';
-import movies from './movies.json' with { type: "json" };
 const search_client = new MeiliSearch({
     host: 'http://localhost:7700',
     apiKey: 'masterkeyforminoritybusiness'
@@ -30,48 +29,64 @@ app.get('/message', (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
-app.post('/users', (req, res) => {
-    if (req.query.username && req.query.email && req.query.age) {
+app.post('/addproduct', (req, res) => {
+
+    if (req.query.ProductID && req.query.Name && req.query.Price && req.query.Description && req.query.URL) {
         console.log('Request received');
         con.connect(function(err) {
-            con.query(`INSERT INTO main.users (username, email, age) VALUES ('${req.query.username}', '${req.query.email}', '${req.query.age}')`, function(err, result, fields) {
+            con.query(`INSERT INTO main.Products (ProductID, Name, Price, Description, URL) VALUES ('${req.query.ProductID}', '${req.query.Name}', '${req.query.Price}', '${req.query.Description}', '${req.query.URL}')`, function(err, result, fields) {
                 if (err) res.send(err);
-                if (result) res.send({username: req.query.username, email: req.query.email, age: req.query.age});
+                if (result) res.send({ProductID: req.query.ProductID, Name: req.query.Name, Price: req.query.Price, Description: req.query.Description, URL: req.query.URL});
                 if (fields) console.log(fields);
             });
         });
     } else {
         console.log('Missing a parameter');
     }
-});
+})
 
-app.get('/users', (req, res) => {
+app.post('/deleteproduct', (req, res) => {
+    if (req.query.Name) {
+        console.log('Request received');
+        con.connect(function(err) {
+            con.query(`DELETE FROM main.Products WHERE ProductID='${req.query.ProductID}'`, function(err, result, fields) {
+                if (err) res.send(err);
+                if (result) res.send({ProductID: req.query.ProductID});
+                if (fields) console.log(fields);
+            });
+        });
+    } else {
+        console.log('Missing a parameter');
+    }
+})
+
+app.get('/products', (req, res) => {
     con.connect(function(err) {
-        con.query(`SELECT * FROM main.users`, function(err, result, fields) {
+        con.query(`SELECT * FROM main.Products`, function(err, result, fields) {
             if (err) res.send(err);
-            if (result) res.json({ result: 'hi' });;
+            if (result) res.json({ result: result });;
         });
     });
 });
 
-app.get('/search/movies/:movie_name/', (req, res) => {
-    // http://localhost:8000/search/movies/spongebob
-    search_client.index('movies').addDocuments(movies)
-    search_client.index('movies').updateFilterableAttributes([
-        'genres',
+app.get('/search/products/:product_name', (req, res) => {
+    // http://localhost:8000/search/products/
+    search_client.index('ProductID').updateFilterableAttributes([
+        'Name',
+        'Description'
     ])
-    search_client.index('movies').updateSortableAttributes([
-        'id',
-        'title'
+    search_client.index('ProductID').updateSortableAttributes([
+        'Name',
+        'Price'
     ])
+    search_client.getIndex('ProductID');
+
     search_client.getTask(0);
+    const search_tag = req.params.product_name;
 
-    const movie_name = req.params.movie_name;
-    const search_tag = movie_name;
-
-    search_client.index('movies').search(search_tag, {
-        limit: 2,
-        sort: ['id:asc'],
+    search_client.index('ProductID').search(search_tag, {
+        limit: 5,
+        sort: ['Price:asc'],
     }).then((r) => {
         res.send(r.hits);
     });
