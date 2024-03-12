cfbbba3ad77aaca593cea5e4d7212d5e50cb1d57
Mae Kyaw San <maeks@Maes-Laptop.local>
2024-03-12 13:58:25 -0400

    a

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
 import Search from "./Search.tsx";
 
-=======
->>>>>>> main
 const Nav = () => {
     return(
         <nav>
             <ul>
-<<<<<<< HEAD
                 <li><Search></Search></li>
-=======
->>>>>>> main
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/contact">Contact</a></li>
